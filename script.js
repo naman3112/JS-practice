eafb925b380292566d99e02ca9cbf4aa090d46b7
Naman <namanarora311@gmail.com>
2021-03-07 13:52:39 +0530

    read,write, and exist

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hello world");
+#!/usr/bin/env node
+const fs = require('fs');
+//console.log("hello world");
+fs.writeFileSync("abc.text","Hello, pepcoder");
+let data= fs.readFileSync("abc.text","utf-8");
+console.log(data);
+console.log(fs.existsSync("abc2.text"));
