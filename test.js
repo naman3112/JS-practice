bc30b0c790a860ab72acb57f0d5a016bddc3f4f9
Naman <namanarora311@gmail.com>
2021-04-06 15:59:59 +0530

    status saved

---
+++ test.js
@@ -0,0 +1,43 @@
+// const fs = require('fs');
+
+// fs.writeFileSync("abc.txt", "Hello");
+// console.log( fs.existsSync("abc.txt"));
+
+//let data = fs.readFileSync("abc.txt","utf-8");
+//  console.log( fs.existsSync("abc.txt"));
+//console.log(data);
+// let cmds = process.argv.slice(2);
+// const fs = require("fs");
+// console.log(cmds);
+// let data = fs.readFileSync(cmds[0],"utf-8");
+// // data=JSON.stringify(data);
+// // data.forEach(element => console.log(element));
+// let count=0;
+// for(let i =0;i<data.length;i++){
+//     if(data[i]==='n'){
+//         count++;
+//     }
+// }
+// console.log('count of n is' + count);
+
+let k = {
+  newObj: {
+      obj2:{
+          obj5:{
+              one:1,
+          },
+      },
+  },
+obj3:{
+    obj4:{two:2},
+},
+
+};
+let s ='';
+function print(k){
+let ans = k.map()
+
+
+
+
+}
